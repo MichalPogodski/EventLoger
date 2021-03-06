22d5a4e617aafb4d1f9e2db7569b1edeb9a8604e
MichalPogodski <pogodskimichal@gmail.com>
2021-03-06 14:21:45 +0100

    2nd backup (erase subscriber)

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include<string>
 #include<vector>
 #include <algorithm>
+#include <typeinfo>
 
 using namespace std;
 
@@ -23,9 +24,9 @@ class FileWriter: public DataWriter{
         string path;
 
         void WriteData(int ID, string info/*, string path*/){
-            path = string(get_current_dir_name()) + "/Data/Events";
+            path = string(get_current_dir_name()) + "/Data/Events.txt";
             txtfile.open(path, fstream::app);
-            txtfile <<ID << ", "<<info<<"\n";
+            txtfile <<ID << ", "<<info <<endl;
             txtfile.close();
         }
 
@@ -34,21 +35,33 @@ class FileWriter: public DataWriter{
 
 class DataBaseWriter: public DataWriter{
     public:
-        // zapisywanie bledow DB do pliku txt
+        ofstream txtfile;
+        string path;
 
         void WriteData(int ID, string info/*, string path*/){
             sqlite3 *db;
             sqlite3_open("Data/Events.db", & db);
+             path = string(get_current_dir_name()) + "/Data/DataBaseErrors.txt";
 
             string createQuery = "CREATE TABLE IF NOT EXISTS events (userid INTEGER, info TEXT);";
             sqlite3_stmt *createStmt;
             sqlite3_prepare(db, createQuery.c_str(), createQuery.size(), &createStmt, NULL);
-            if (sqlite3_step(createStmt) != SQLITE_DONE) cout << "Didn't Create Table!" << endl;
+            if (sqlite3_step(createStmt) != SQLITE_DONE){
+                cout << "Didn't Create Table!" << endl;
+                txtfile.open(path, fstream::app);
+                txtfile <<"Didn't Create Table!" << endl;
+                txtfile.close();
+            }
 
             string insertQuery = "INSERT INTO events (userid, info) VALUES (" + to_string(ID) + ", '" + info + "');";
             sqlite3_stmt *insertStmt;
             sqlite3_prepare(db, insertQuery.c_str(), insertQuery.size(), &insertStmt, NULL);
-            if (sqlite3_step(insertStmt) != SQLITE_DONE) cout << "Didn't Insert Item!" << endl;
+            if (sqlite3_step(insertStmt) != SQLITE_DONE){
+                cout << "Didn't Insert Item!" << endl;
+                txtfile.open(path, fstream::app);
+                txtfile <<"Didn't Insert Item! InstertQuety: " << insertQuery <<endl;
+                txtfile.close();
+            }
         }
 
 };
@@ -69,7 +82,12 @@ public:
     }
 
     void EraseSubscriber(DataWriter* sub){
-        subscribers.erase(remove(subscribers.begin(), subscribers.end(), sub), subscribers.end());
+//        subscribers.erase(remove(subscribers.begin(), subscribers.end(), sub), subscribers.end());
+        for (int i = 0; i < subscribers.size(); ++i) {
+            if (typeid(subscribers[i]).name() == typeid(sub).name()){
+                subscribers.erase(subscribers.begin()+i);
+            }
+        }
     }
 
 };
@@ -82,7 +100,8 @@ int main(){
     EventLoger.AddSubscriber(new DataBaseWriter);
 
     EventLoger.LogToSubscribers(1234, "first pass");
-//    EventLoger.EraseSubscriber(WriteTxt);
+    EventLoger.EraseSubscriber(new FileWriter);
+    EventLoger.LogToSubscribers(12345678, "second pass");
 
 
     return 0;
